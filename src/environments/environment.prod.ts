765f60075a9d885332e7cf1a05c921907b5e7846
HimanshuPandey <himanshugithub6201@gmail.com>
2020-11-07 15:43:46 +0530

    commit for aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,32 +1,36 @@
 export const environment = {
+  //https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/
   production: true,
-  saveUserProfile: "https://dry-forest-73283.herokuapp.com/users/register",
-  authenticate: "https://dry-forest-73283.herokuapp.com/users/authenticate",
+  saveUserProfile:
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/users/register",
+  authenticate:
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/users/authenticate",
   getQuestionsForTestSeries:
-    "https://dry-forest-73283.herokuapp.com/questionLayout/getQuestions",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/questionLayout/getQuestions",
   postSavedAnswer:
-    "https://dry-forest-73283.herokuapp.com/reportDetail/saveReportQuestionWise",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/reportDetail/saveReportQuestionWise",
   postSubmittedAnswer:
-    "https://dry-forest-73283.herokuapp.com/reportOverall/saveOverallReport",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/reportOverall/saveOverallReport",
   getAllCoursesOffered:
-    "https://dry-forest-73283.herokuapp.com/coursesOffered/getAllCoursesOffered",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/coursesOffered/getAllCoursesOffered",
   findAllUnVerifiedUser:
-    "https://dry-forest-73283.herokuapp.com/users/findAllUnVerifiedUser?userId=",
-  createTest: "https://dry-forest-73283.herokuapp.com/coursesDetail/createTest",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/users/findAllUnVerifiedUser?userId=",
+  createTest:
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/coursesDetail/createTest",
   getCourseIdListForAdmin:
-    "https://dry-forest-73283.herokuapp.com/coursesDetail/getCourseIdListForAdmin",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/coursesDetail/getCourseIdListForAdmin",
   questionLayoutUpload:
-    "https://dry-forest-73283.herokuapp.com/questionLayout/upload?courseId=",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/questionLayout/upload?courseId=",
   userVerification:
-    "https://dry-forest-73283.herokuapp.com/users/verification?userId=",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/users/verification?userId=",
   uploadProfileData:
-    "https://dry-forest-73283.herokuapp.com/users/uploadProfileData?userId=",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/users/uploadProfileData?userId=",
   getOverallReportByUserId:
-    "https://dry-forest-73283.herokuapp.com/reportOverall/getOverallReportByUserId?user_id=",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/reportOverall/getOverallReportByUserId?user_id=",
   getTopRank:
-    "https://dry-forest-73283.herokuapp.com/reportOverall/getTopRank?course_id=",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/reportOverall/getTopRank?course_id=",
   getQuestionAnalysis:
-    "https://dry-forest-73283.herokuapp.com/reportDetail/getQuestionAnalysis?user_id=",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/reportDetail/getQuestionAnalysis?user_id=",
   getCoursesDescriptionByExamCode:
-    "https://dry-forest-73283.herokuapp.com/coursesDetail/getCoursesDescriptionByExamCode",
+    "https://egatetutorbalancer-1480320595.ap-south-1.elb.amazonaws.com/coursesDetail/getCoursesDescriptionByExamCode",
 };
