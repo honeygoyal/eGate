943d22464029694f355f245c3b66bbed5e6996c8
Himanshu Pandey <himanshup6201@gmail.com>
2020-12-03 22:31:23 +0530

    dsa

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,43 +1,21 @@
 import { Injectable } from "@angular/core";
-import { Actions, ofType, createEffect } from "@ngrx/effects";
-import { AuthActions } from "./action-types";
-import { tap } from "rxjs/operators";
+import { Actions } from "@ngrx/effects";
 import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions, private router: Router) {
+  constructor(public actions$: Actions, private router: Router) {
     actions$.subscribe((action) => {
       if (action.type == "[Login Page] User Login") {
         localStorage.setItem("user", JSON.stringify(action["user"]));
       }
       if (action.type == "[Top Menu] Logout") {
         localStorage.removeItem("user");
+        localStorage.clear()
         this.router.navigateByUrl("/login");
+        location.reload()
       }
     });
 
-    // const login$ = createEffect(
-    //   () =>
-    //     this.actions$.pipe(
-    //       ofType(AuthActions.login),
-    //       tap((action) => {
-    //         localStorage.setItem("user", JSON.stringify(action.user));
-    //       })
-    //     ),
-    //   { dispatch: false }
-    // );
-
-    // const logout$ = createEffect(
-    //   () =>
-    //     this.actions$.pipe(
-    //       ofType(AuthActions.logout),
-    //       tap((action) => {
-    //         localStorage.removeItem("user");
-    //         this.router.navigateByUrl("/login");
-    //       })
-    //     ),
-    //   { dispatch: false }
-    // );
   }
 }
