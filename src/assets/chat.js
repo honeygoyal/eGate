f3b895ccf0895a52d2588e825d87eca86ab72e7b
Hari Charan <khcharan@gmail.com>
2020-12-19 13:52:27 +0530

    Chat panel disabled in exampanel now

--- src/assets/chat.js
+++ src/assets/chat.js
@@ -1,9 +1,13 @@
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5fae912a7279c47e5dcf981f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+if(location.href.indexOf("exampanel") > -1 ){
+   console.log("Disabling chat panel in exam panel");
+}else{
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5fae912a7279c47e5dcf981f/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+}    
 })();
